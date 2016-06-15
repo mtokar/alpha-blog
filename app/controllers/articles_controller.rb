763059486fab06f3169f3331c6af088a921d886f
MT <miketokar@hotmail.com>
2016-06-15 02:19:40 +0000

    Clear reduncancies

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,7 @@
 class ArticlesController < ApplicationController
   
+  before_action :set_article, only: [:edit, :update, :show, :destroy]
+  
   def index
     @articles = Article.all
   end
@@ -9,7 +11,7 @@ class ArticlesController < ApplicationController
   end
   
   def edit
-    @article = Article.find(params[:id])
+    
   end
   
   def create
@@ -32,8 +34,6 @@ class ArticlesController < ApplicationController
   
   def update
     
-    @article = Article.find(params[:id])
-    
     if @article.update(article_params)
       
       flash[:notice] = "Article was updated"
@@ -49,13 +49,13 @@ class ArticlesController < ApplicationController
   
   def show
     
-    @article = Article.find(params[:id])
+    
     
   end
   
   def destroy
   
-    @article = Article.find(params[:id])  
+      
     @article.destroy
     flash[:notice] = "Article deleted"
     redirect_to articles_path
@@ -63,6 +63,10 @@ class ArticlesController < ApplicationController
   end
 
   private
+  
+    def set_article
+      @article = Article.find(params[:id])
+    end
 
   def article_params
 
